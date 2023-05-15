488e46e8608330d664d435beb0f10867c313189f
David Lane <git2018@dmlane.net>
2023-05-15 12:59:28 +0100

    Fixed some publish issues

---
+++ .run/commands/poetrypublish.sh
@@ -0,0 +1,27 @@
+# vim: set ft=bash ts=3 sw=3 expandtab:
+# Build release artifacts into the dist/ directory
+
+command_poetrypublish() {
+   echo "Pushing release to fury.io ..."
+
+   package=$(poetry version|cut -d " " -f 1)
+   poetry publish --repository fury-pub
+   if [ $? != 0 ]; then
+      echo "*** Publish failed"
+      exit 1
+   fi
+   
+   private_count=$(fury list|grep -c private)
+   if [ $private_count -gt 1 ] ; then
+      highlight ">1 private packages - ^please fix"
+      fury list
+   else
+      fury versions $package
+   fi
+
+
+
+
+   echo "done"
+}
+
